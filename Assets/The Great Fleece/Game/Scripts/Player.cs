0735c402fd0550340a1f2307e0434d87366a2912
fernandoalcasan <fernandoalcasan@outlook.com>
2021-07-12 20:20:15 -0500

    Player idle and walk animations implemented

--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -6,12 +6,17 @@ using UnityEngine.AI;
 public class Player : MonoBehaviour
 {
     private NavMeshAgent _navAgent;
+    private Animator _anim;
 
     void Start()
     {
         _navAgent = GetComponent<NavMeshAgent>();
         if (_navAgent is null)
             Debug.LogError("NavMeshAgent is NULL");
+
+        _anim = GetComponentInChildren<Animator>();
+        if (_anim is null)
+            Debug.LogError("Animator in children is NULL");
     }
 
     void Update()
@@ -19,10 +24,16 @@ public class Player : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hitInfo, 100f))
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
             {
                 _navAgent.SetDestination(hitInfo.point);
             }
         }
+
+        if(_navAgent.velocity.sqrMagnitude < 1f)
+            _anim.SetBool("Walk", false);
+        else
+            _anim.SetBool("Walk", true);
+
     }
 }
