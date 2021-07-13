3fb1add4a057ab2c75a75b1463d033c41fd4469c
fernandoalcasan <fernandoalcasan@outlook.com>
2021-07-13 16:19:07 -0500

    Waypoints and animations implemented for Guards

---
+++ Assets/The Great Fleece/Game/Scripts/AIGuard.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIGuard : MonoBehaviour
+{
+    private NavMeshAgent _navAgent;
+    private Animator _anim;
+
+    [SerializeField]
+    private List<Transform> _waypoints;
+    private int _target;
+
+    void Start()
+    {
+        _navAgent = GetComponent<NavMeshAgent>();
+        if (_navAgent is null)
+            Debug.LogError("Nav Mesh Agent is NULL");
+
+        _anim = GetComponent<Animator>();
+        if (_anim is null)
+            Debug.LogError("Animator is NULL");
+
+        if (_waypoints.Count > 1 && !(_waypoints[0] is null))
+            StartCoroutine(WaitToPatrol());
+        else
+            Debug.LogError("Please select 2 waypoints at least");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Waypoint"))
+        {
+            _target++;
+
+            if (_target == _waypoints.Count)
+            {
+                _waypoints.Reverse();
+                _target = 1;
+                StartCoroutine(WaitToPatrol());
+            }
+            else
+                SetNextTarget();
+        }
+    }
+
+    private void SetNextTarget()
+    {
+        if (!(_waypoints[_target] is null))
+            _navAgent.SetDestination(_waypoints[_target].position);
+        else
+            Debug.LogError("Target waypoint is null");
+    }
+
+    private IEnumerator WaitToPatrol()
+    {
+        _anim.SetBool("Walk", false);
+        yield return new WaitForSeconds(Random.Range(2f, 5f));
+        _anim.SetBool("Walk", true);
+        SetNextTarget();
+    }
+}
