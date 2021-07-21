ccb4a23f8db9eb3f2bb1dce698cfca2c99eb8a63
fernandoalcasan <fernandoalcasan@outlook.com>
2021-07-21 14:54:23 -0500

    UI Manager implemented with cutscene buttons

---
+++ Assets/The Great Fleece/Game/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance is null)
+                Debug.LogError("UI manager is NULL");
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
